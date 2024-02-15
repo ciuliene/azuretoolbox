47dd909d074e465ef17e2b5edc03f8f54cefc7b9
ciuliene <errgioul2@gmail.com>
2024-02-15 13:44:51 +0100

    Command for db and covert datetime to string

--- src/azuretoolbox/database.py
+++ src/azuretoolbox/database.py
@@ -1,4 +1,5 @@
 from pyodbc import connect
+from datetime import datetime
 
 
 class Database:
@@ -6,7 +7,7 @@ class Database:
         self._conn = None
         pass
 
-    def connect(self, server: str, database: str, username: str, password: str):
+    def connect(self, server: str, database: str, username: str, password: str) -> bool:
         conn_str = "DRIVER={ODBC Driver 18 for SQL Server};" \
             "SERVER=" + server + ";" \
             "DATABASE=" + database + ";" \
@@ -19,7 +20,7 @@ class Database:
         self._conn = connect(conn_str)
         return True
 
-    def disconnect(self):
+    def disconnect(self) -> bool:
         if self._conn is not None:
             self._conn.close()
             self._conn = None
@@ -30,12 +31,19 @@ class Database:
         for row in response:
             temp = {}
             for i in range(len(header)):
-                temp[header[i][0]] = row[i]
+                temp[header[i][0]] = row[i] if type(
+                    row[i]) != datetime else row[i].strftime('%Y-%m-%d %H:%M:%S')
             result.append(temp)
         return result
 
-    def query(self, query: str):
+    def query(self, query: str) -> list[dict]:
         cursor = self._conn.cursor()
         cursor.execute(query)
         response = cursor.fetchall()
         return self.__parse__(response, cursor.description)
+
+    def command(self, query: str) -> bool:
+        cursor = self._conn.cursor()
+        cursor.execute(query)
+        self._conn.commit()
+        return True
